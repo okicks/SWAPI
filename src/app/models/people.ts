9bcb65d5512cdba3cfb0f158b0ea95e70c80e891
olihu822 <52262030+olihu822@users.noreply.github.com>
2019-10-28 22:47:55 -0400

    people model

---
+++ src/app/models/people.ts
@@ -0,0 +1,18 @@
+export interface People{
+    Name: string;
+    Height?: number;
+    Mass?: number;
+    HairColor: string;
+    SkinColor: string;
+    EyeColor: string;
+    BirthYear?: string;
+    Gender?: string;
+    Homeworld?: string;
+    Films: string;
+    Species: string;
+    Vehicles?: string;
+    Starships?: string;
+    CreatedUtc: Date;
+    EditedUtc: Date;
+    Url: string;
+}
